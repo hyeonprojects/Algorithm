73c716fcb61bb8058c22036ee42ea668311476a1
hyeonproject <axzswq@gmail.com>
2022-10-22 10:06:07 +0900

    create: 코테 공부

--- array/product_of_array_except_self.py
+++ array/product_of_array_except_self.py
@@ -1,1 +1,11 @@
+import re
 
+stack1 = []
+stack2 = []
+stack3 = []
+
+name = re.sub(r'[a-zA-Z]', 'X', '꽃미남하하하')
+print(name)
+
+if (not stack1) and (not stack2) and (not stack3):
+    print('test')
